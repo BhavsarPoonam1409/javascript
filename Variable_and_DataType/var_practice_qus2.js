e1e4c196209bf3091fbfad4088cef078d3d1b5c4
BhavsarPoonam1409 <bhavsarpoonam46@gmail.com>
2026-05-24 09:57:28 +0530

    practice qustion

---
+++ Variable_and_DataType/var_practice_qus2.js
@@ -0,0 +1,12 @@
+const profile = {
+    userName : "Poonam Bhavsar...",
+    isFollow : true,
+    Message : "Message",
+    post : 195,
+    followers : 50000,
+    following : 70,
+    Bio : "POONAM BHAVSAR"
+
+};
+
+console.log(profile);
